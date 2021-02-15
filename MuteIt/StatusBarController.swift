7cd5b166a99b653f41e7f1c62568c603d03288f2
choopong <cchoopong@gmail.com>
2021-02-15 14:11:36 +0700

    when unmute it can restore original volume before mute

--- MuteIt/StatusBarController.swift
+++ MuteIt/StatusBarController.swift
@@ -12,6 +12,7 @@ class StatusBarController {
     private var statusBar: NSStatusBar
     private var statusItem: NSStatusItem
     private var isMute: Bool
+    private var unMuteVolume: Float
     private var statusBarButton: NSStatusBarButton
     
     init() {
@@ -20,7 +21,8 @@ class StatusBarController {
         // Creating a status bar item having a fixed length
         statusItem = statusBar.statusItem(withLength: 28.0)
         isMute = true
-        setInpuLevel(level: 0)
+        unMuteVolume = 0.5
+        setInpuLevel(isMute: isMute)
         
         if let statusBarButton = statusItem.button {
             self.statusBarButton = statusBarButton
@@ -43,19 +45,19 @@ class StatusBarController {
                 self.statusBarButton.image = NSImage(named: "UnmuteIcon")
                 self.statusBarButton.image?.size = NSSize(width: 18.0, height: 18.0)
                 self.statusBarButton.image?.isTemplate = true
-                setInpuLevel(level: 1)
+                setInpuLevel(isMute: isMute)
             }
             else {
                 isMute = true
                 self.statusBarButton.image =  NSImage(named: "MuteIcon")
                 self.statusBarButton.image?.size = NSSize(width: 18.0, height: 18.0)
                 self.statusBarButton.image?.isTemplate = true
-                setInpuLevel(level: 0)
+                setInpuLevel(isMute: isMute)
             }
         }
     }
     
-    func setInpuLevel(level: Int) {
+    func setInpuLevel(isMute: Bool) {
         // Get default audio inout id
         var deviceID = AudioDeviceID(0)
         var deviceIDSize = UInt32(MemoryLayout.size(ofValue: deviceID))
@@ -73,15 +75,32 @@ class StatusBarController {
             &deviceIDSize,
             &deviceID)
         
-        // Set Volume
-        var volume = Float32(level)
-        let volumeSize = UInt32(MemoryLayout.size(ofValue: volume))
-        
+        var volume = Float32(0)
+        var volumeSize = UInt32(MemoryLayout.size(ofValue: volume))
         var volumePropertyAddress = AudioObjectPropertyAddress(
             mSelector: kAudioDevicePropertyVolumeScalar,
             mScope: kAudioDevicePropertyScopeInput,
             mElement: kAudioObjectPropertyElementMaster)
         
+        if(isMute) {
+            // Get Volume
+            AudioObjectGetPropertyData(
+                deviceID,
+                &volumePropertyAddress,
+                0,
+                nil,
+                &volumeSize,
+                &volume)
+            self.unMuteVolume = volume
+            volume = 0
+        } else {
+            if self.unMuteVolume == 0 {
+                self.unMuteVolume = 0.5
+            }
+            volume = self.unMuteVolume
+        }
+        
+        // Set Volume
         AudioObjectSetPropertyData(
             deviceID,
             &volumePropertyAddress,
@@ -90,8 +109,8 @@ class StatusBarController {
             volumeSize,
             &volume)
         
-        // Set Mute
-        var mute = Int32(0)
+        // Set Mute/Unmute
+        var mute = 0
         if(volume == 0) {
             mute = 1
         }
